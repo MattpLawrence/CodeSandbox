7671cca4dfb28e6e54942e05b7230838b6154678
Matthew Lawrence <86316374+MattpLawrence@users.noreply.github.com>
2022-03-02 14:40:44 -0500

    add basic file structure Added basic file structure, and cleaned out some parts of the initial React template.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles.css";
+import Page from "./components/Page";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Page />
     </div>
   );
 }
