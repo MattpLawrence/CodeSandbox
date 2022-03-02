787472cc1ed188eb5428746a78ef4a0ccc7a6f30
Matthew Lawrence <86316374+MattpLawrence@users.noreply.github.com>
2022-03-02 15:10:57 -0500

    clean up files, add key to list item clean up the unused declarations.

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 function List(props) {
   console.log(props.urlList);
   return props.urlList.map((item, i) => (
-    <div>
+    <div key={i}>
       <div key={item.id} onClick={() => props.addListItem(item.id)}>
         {item.fullUrl}
+        {item.shortUrl}
       </div>
 
       <p onClick={() => props.removeListItem(item.id)}> 🗑️</p>
